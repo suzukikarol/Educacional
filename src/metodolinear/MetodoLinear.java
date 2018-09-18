ef8b7df316e2654afe0558e6d6f20f6ceb01052c
alunocmc <alunocmc@1222LAB008.umc.br>
2018-09-18 10:52:13 -0300

    Criação da aula de hoje sobre método dos M.Quadrados, no caso mátedo linear. já finalizado mostrando valores de a e b.

--- src/metodolinear/MetodoLinear.java
+++ src/metodolinear/MetodoLinear.java
@@ -5,9 +5,11 @@
  */
 package metodolinear;
 
+import java.util.Scanner;
+
 /**
  *
- * @author alunocmc
+ * @author suzukiKarol
  */
 public class MetodoLinear {
 
@@ -16,6 +18,42 @@ public class MetodoLinear {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[] vetX = new double[6];
+        double[] vetY = new double[6];
+        
+        int n;
+        
+        double sx=0, sy=0, sxy=0, sx2=0, delta=0, deltaA=0, deltaB=0, a=0,b=0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite n: ");n=sc.nextInt();
+        
+        for (int i=1; i<=n; i++){
+            System.out.println("Digite x["+1+"]: "); vetX[1]=sc.nextDouble();
+            }
+        
+        for (int i=1; i<=n; i++){
+            System.out.println("Digite y["+1+"]: "); vetY[1]=sc.nextDouble();
+            }
+        
+        for(int i=1; i<=n; i++){
+            sx += vetX[i];
+            sy += vetY[i];
+            sxy += vetX[i]*vetY[i];
+            sx2 += vetX[i]*vetY[i];
+        }
+        
+        delta = sx2*n-sx*sx;
+        deltaA = sxy*n-sx*sy;
+        deltaB = sx2*sy-sxy*sx;
+        
+        a = deltaA/delta;
+        b = deltaB/delta;
+        
+        System.out.println("\n\nf(x)= "+a+" x + (" +b+ ")");
+        
+         
     }
     
 }
